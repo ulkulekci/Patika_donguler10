398aa7fe64f17bda74af499bad06b5f4fd382ac7
Ulku Kulekci <87547007+ulkulekci@users.noreply.github.com>
2021-12-31 18:12:46 +0300

    Add files via upload

--- java_Donguler10/src/Main.java
+++ java_Donguler10/src/Main.java
@@ -5,19 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("EBOB EKOK bulma\n iki sayi giriniz:");
-        int i,ebob =1;
+        int i=1, ebob =1;
         int a= input.nextInt();
         int b= input.nextInt();
-     if (a>b){for (i=1;i<=a;i++){if (a%i==0 && b%i==0)
-      ebob=i;
+     if (a>b){while (i<=a){
+         {if (a%i==0 && b%i==0)
+             ebob=i;} i++;
      }
 
          System.out.println("EBOB:"+ ebob);
          System.out.print("EKOK:"+a*b/ebob);
      }
-     if (b>a){
-     for (i=1;i<=b;i++){if (a%i==0 && b%i==0)
-            ebob=i;}
+     else { i=1; ebob=1;
+     while (i<=b){if (a%i==0 && b%i==0)
+            ebob=i;} i++;
         System.out.println("EBOB:"+ ebob);
          System.out.print("EKOK:"+a*b/ebob);
      }
